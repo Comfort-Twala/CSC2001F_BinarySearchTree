21da77e85174711712f8ff5ebefb6ad7ed5cb004
Comfort-Twala <kontreitroos@gmail.com>
2021-04-06 21:17:23 +0200

    Analyser code added

--- script/analyser.py
+++ script/analyser.py
@@ -7,32 +7,66 @@ Python script to:
 """
 
 def main():
-    pass
+    for data_struc in ["Array", "BST"]:
+        for n in range(500, 5001, 500):
+            expfile = open_expfile(f'data/experiment/{data_struc}_subset_n_{n}.txt')
+            valueList = count_val(expfile)
+            recordData(best_case(valueList), worst_case(valueList), average(valueList), data_struc, n)
 
 def open_expfile(file):
     """
     Function to open experiment file and return results as a list
     """
+    filelist = []
+    with open(file, 'r') as f:
+        for line in f:
+            filelist.append(line)
+        f.close()
+    return filelist
 
-def count_val(list):
+def count_val(file_list):
     """
     Function to store all the operation count values from given list and return as a list
     """
+    countlist = []
+    for entry in file_list:
+        try:
+            countlist.append(int(entry))
+        except:
+            pass
+    return countlist
 
-def best_case(list):
+def best_case(c_list):
     """
     Funtion to determine the minimum of the count values
     """
+    return min(c_list)
 
-def worst_case(list):
+def worst_case(c_list):
     """
     Funtion to determine the maximum of the count values
     """
+    return max(c_list)
 
-def average(list):
+def average(c_list):
     """
     Funtion to determine the average of the count values
     """
+    return sum(c_list)/len(c_list)
+
+def recordData(min, max, avr, type, n):
+    """
+    Function that takes in minimum, maximum and average for count and writes it to a file.
+    """
+    with open(f'data/experiment/analysis/{type}_n_{n}.txt', 'w') as f:
+        f.write(f"Analysis of {type}App when n is {n}:")
+        f.write("Minimum (Best Case):\n")
+        f.write(min, '\n')
+        f.write("Maximum (Worst Case):\n")
+        f.write(max, '\n')
+        f.write("Average:\n")
+        f.write(avr)
+        f.close()
 
 if __name__ == "__main__":
-    pass
+    main()
