5a150e6fd666221dc28fcfad509402b4bdb4bebf
Comfort-Twala <kontreitroos@gmail.com>
2021-04-06 20:16:25 +0200

    analyser skeleton added

---
+++ script/analyser.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Python script to:
+    * Open all experiment results files
+    * Store all operation count values.
+    * Determine the minimum (best case), maximum (worst case) and average of these count values. 
+"""
+
+def main():
+    pass
+
+def open_expfile(file):
+    """
+    Function to open experiment file and return results as a list
+    """
+
+def count_val(list):
+    """
+    Function to store all the operation count values from given list and return as a list
+    """
+
+def best_case(list):
+    """
+    Funtion to determine the minimum of the count values
+    """
+
+def worst_case(list):
+    """
+    Funtion to determine the maximum of the count values
+    """
+
+def average(list):
+    """
+    Funtion to determine the average of the count values
+    """
+
+if __name__ == "__main__":
+    pass
