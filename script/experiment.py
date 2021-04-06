cc858d6a10ae3f28323d4fd4d9533d135bc30092
Comfort-Twala <kontreitroos@gmail.com>
2021-04-06 12:07:37 +0200

    Added python script skeleton

---
+++ script/experiment.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Python script to:
+    * Create a subset of n entries from sample data
+    * Run both instrumented applications for every one of the n parameters corresponding to the subset of the data file.  Store all operation count values.
+    * Determine the minimum (best case), maximum (worst case) and average of these count values. 
+"""
+
+### Imports
+import os
+import subprocess
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+
+
+def main(app):
+    """
+    Main method to run expiriment.
+    paramenter - App on which to run the experiment on.
+    """
+    pass
+
+def create_subset(n):
+    """
+    Create a subset of n entries from sample data
+    """
+    pass
+
+def count_values(count):
+    """
+    Add value of counter to dictionary
+    """
+    pass
+
+def open_datafile():
+    """
+    opens datafile
+    """
+    pass
+
+
+if __name__ == "__main__":
+    pass
