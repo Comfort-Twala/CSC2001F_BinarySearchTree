9fabe87d89de28f218657aa29440e4ec1100add1
Comfort-Twala <kontreitroos@gmail.com>
2021-04-07 00:26:37 +0200

    added grapher script to create csv data stats and graph them

---
+++ script/grapher.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Python script to:
+    * Open analysed files and get array of [min, max, average]
+    * Create DataFrame for Array- and BSTApp results
+    * Create Graphs from data  
+"""
+
+## Imports
+import subprocess
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+    """
+    Main method to initiate graphing
+    """
+
+def getdata(file):
+    """
+    Open file and return array of [min, max, average]
+    """
+
+def data_frame_create(data):
+    """
+    Create a DataFrame with all the data
+    """
+
+def data_frame_csv(dataFrame):
+    """
+    Create csv file from DataFrame
+    """
+
+def graphData(dataFrame):
+    """
+    Plot a graph with data from data frame
+    """
+
+if __name__ == "__main__":
+    main()
